d1c509698fa57e4eb0900cadf164790d4e2b3f30
acm <nmblunier@gmail.com>
2018-02-06 15:08:28 -0600

    #32 #33 Noah

---
+++ Index.php
@@ -0,0 +1,6 @@
+<form action="Print.php" method="POST">
+	<textarea rows="20" cols="50" name="Print_This">
+		</textarea>
+	<br />
+	<input type="submit" name="Submit">
+	</form>
---
+++ Print.php
@@ -0,0 +1,8 @@
+<?php
+
+if ($_POST['Submit']) {
+	$message=$_POST['Print_This'];
+	echo $message;
+}
+
+?>
